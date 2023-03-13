431372336a1d1df891f1b7e9d766f9de236d2b33
Muskanthemiracle <125651190+Muskanthemiracle@users.noreply.github.com>
2023-03-13 15:14:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<link rel="stylesheet" href="home.css">
+</head>
+<body>
+<div >
+<img id="pic" src="pglife.png">
+<a href="login.php"><input class="sb" type="Submit" name="Login" value="Login"/></a>
+<a href="registration.php"><input class="sb" type="Submit" name="Signup" value="Signup"/></a>
+</div>
+<div id="pc">
+<marquee><img class="ab" src="pgpic.webp"><img class="ab" src="download.jpg"><img class="ab" src="pgr.webp"> </marquee>
+</div>
+<div id="im" >
+<h1 id="h">
+Major Cities
+</h1>
+<div id="bt"> <BUTTON class="bt"type="SUBMIT" name="imbutton" value="click"><a href="delhirooms.php"><img class="is" src="dlhi.webp" ></a></BUTTON>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+<BUTTON class="bt"type="SUBMIT" name="imbutton" value="click"><a href="beng.php"><img class="is" src="beng.jpg" ></a></BUTTON>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+<BUTTON class="bt"type="SUBMIT" name="imbutton" value="click"><a href="hyder.php"><img class="is" src="hydra.jpg" ></a></BUTTON>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;
+<BUTTON class="bt"type="SUBMIT" name="imbutton" value="click"><a href="mumb.php"><img class="is" src="mumbai.jpg" ></a></BUTTON></div>
+<div id="footer">
+<footer id="foot">
+<?php
+include'Footer.php';
+?>
+</footer>
+
+
+</div>
+
+</body>
+</html>
